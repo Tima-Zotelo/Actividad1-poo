74bdc0bc4c217b50ce6bf7d7c08a8e13ebc36ea6
Tima-Zotelo <101579323+Tima-Zotelo@users.noreply.github.com>
2023-04-26 19:57:33 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,30 @@ from menu import Menu
 from ClassEmail import Email
 import os
 
+def test():
+    print('------ TEST ------')
+    print('datos a probar: \nDatos correctos: \ninformatica.fcefn@gmail.com, wicc2019@unsj-cuim.edu, juanLiendro1957@yahoo.com \n\nDatos incorrectos: \njulio3312gmail.com, ernesto@yahoo')
+    nuevotest= Email()
+    print('\n\n--- DATOS CORRECTOS')
+    print('informatica.fcefn@gmail.com')
+    nuevotest.crearCuenta('informatica.fcefn@gmail.com','contrasena1')
+    print('wicc2019@unsj-cuim.edu')
+    nuevotest.crearCuenta('wicc2019@unsj-cuim.edu','contrasena2')
+    print('juanLiendro1957@yahoo.com')
+    nuevotest.crearCuenta('juanLiendro1957@yahoo.com','contrasena3')
+    print('\n--- DATOS INCORRECTOS')
+    print('julio3312gmail.com')
+    nuevotest.crearCuenta('julio3312gmail.com','contrasena4')
+    print('ernesto@yahoo')
+    nuevotest.crearCuenta('ernesto@yahoo','contrasena5')
+    os.system('Pause')
+    os.system('cls')
+
 if __name__ == '__main__':
+    op=int (input('Desea ejecutar el test? \n 1 - SI \n 2 - NO\n'))
+    if op==1:
+        test()
+
     auxEmail = Email()
     xmenu = Menu()
     op = xmenu.mostrarMenu()
